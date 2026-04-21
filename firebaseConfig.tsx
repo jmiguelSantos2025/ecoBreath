e55e817a16ecdc1043a2675563e5e7eb6d32ed59
João Miguel <jmiguel2021nt@gmail.com>
2026-04-20 23:28:53 -0400

    Update firebaseConfig.tsx to use environment variables for Firebase credentials.

--- firebaseConfig.tsx
+++ firebaseConfig.tsx
@@ -1,23 +1,13 @@
+// firebaseConfig.tsx
 
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import {getAuth, GoogleAuthProvider} from 'firebase/auth';
-import { getDatabase, ref, onValue, get } from "firebase/database";
-import {getFirestore} from 'firebase/firestore';
 const firebaseConfig = {
-  apiKey: "AIzaSyDhKNpY7T9ks51iqF1JRizq0x92G2bkgsU",
-  authDomain: "ecobreathdatabase.firebaseapp.com",
-  projectId: "ecobreathdatabase",
-  storageBucket: "ecobreathdatabase.firebasestorage.app",
-  messagingSenderId: "441862536106",
-  appId: "1:441862536106:web:0c09f7560aab54b54289d3",
-  measurementId: "G-KC56S187ME"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const auth = getAuth(app);
-const database = getDatabase(app);
-const firestore = getFirestore(app);
-const provider = new GoogleAuthProvider();
-export {auth, database, firestore,provider};
+export default firebaseConfig;
